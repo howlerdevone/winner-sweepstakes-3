9befeebe3fb533d7e3e9a80bf5aa4ecee0069356
howlerdev506 <ppedroza558@gmail.com>
2025-09-07 15:33:59 -0600

    deploy yo github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/winner-sweepstakes-3' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/winner-sweepstakes-3' : '',
 };
 
 export default nextConfig;
